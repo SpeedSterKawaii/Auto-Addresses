e002e5edd013659d706812f8289b3be03de72b97
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-09-28 08:54:13 -0400

    Update Console.cpp

--- Console.cpp
+++ Console.cpp
@@ -2,3 +2,19 @@
   Auto addresses will print out the address in console format.
   This is NOT a .DLL file, this is a .EXE console application!
 */
+
+#include <iostream>
+#include <Windows.h> //need for some functions
+using namespace std;
+
+int main()//works only on console
+{
+    SetConsoleTitleA("Auto Addresses Console // By SpeedSterKawaii");
+    Sleep(1000);//wait a second before getting offsets
+    cout << "Wait, replacing latest addresses" << endl; 
+    Sleep(10);
+    cout << "This will display in int (not hex) format" << endl; 
+    //it will show the int's (NOT IN HEX FORMAT)
+   
+   return NULL;
+}
